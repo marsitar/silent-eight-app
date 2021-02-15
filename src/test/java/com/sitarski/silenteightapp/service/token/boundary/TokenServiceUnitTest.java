18de304851714beda05e09efd8704c0ffceb87d2
marek sitarski <marek_sitarski@vp.pl>
2021-02-15 23:29:12 +0100

    ref #0003 - TokenMapperUnitTest class - optimize imports

--- src/test/java/com/sitarski/silenteightapp/service/token/boundary/TokenServiceUnitTest.java
+++ src/test/java/com/sitarski/silenteightapp/service/token/boundary/TokenServiceUnitTest.java
@@ -5,11 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import java.util.Collections;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 class TokenServiceUnitTest {
 
