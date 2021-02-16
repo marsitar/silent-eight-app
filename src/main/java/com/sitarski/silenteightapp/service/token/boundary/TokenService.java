ab5cc15b8e0f7e3207421da601cd97a0dcd010ca
marek sitarski <marek_sitarski@vp.pl>
2021-02-16 19:22:53 +0100

    ref #0004 - implement getTokens() method in service, concat two lists in it, add mapper class

--- src/main/java/com/sitarski/silenteightapp/service/token/boundary/TokenService.java
+++ src/main/java/com/sitarski/silenteightapp/service/token/boundary/TokenService.java
@@ -1,4 +1,43 @@
 package com.sitarski.silenteightapp.service.token.boundary;
 
+import com.sitarski.silenteightapp.common.entity.GenderType;
+import com.sitarski.silenteightapp.repository.token.boundary.TokenRepository;
+import com.sitarski.silenteightapp.repository.token.entity.Token;
+import com.sitarski.silenteightapp.rest.token.entity.TokenDto;
+import com.sitarski.silenteightapp.service.token.control.TokenMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+@RequestScope
 public class TokenService {
+
+    private final TokenRepository tokenRepository;
+    private final TokenMapper tokenMapper;
+
+    @Autowired
+    public TokenService(TokenRepository tokenRepository, TokenMapper tokenMapper) {
+        this.tokenRepository = tokenRepository;
+        this.tokenMapper = tokenMapper;
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(TokenService.class);
+
+
+    public List<TokenDto> getTokens() {
+        List<Token> femaleTokens = tokenRepository.findAllByGender(GenderType.FEMALE);
+        List<Token> maleTokens = tokenRepository.findAllByGender(GenderType.MALE);
+        List<TokenDto> tokens = Stream.concat(femaleTokens.stream(), maleTokens.stream())
+                .map(tokenMapper::mapToTokenDto)
+                .collect(Collectors.toList());
+        return tokens;
+    }
+
 }
