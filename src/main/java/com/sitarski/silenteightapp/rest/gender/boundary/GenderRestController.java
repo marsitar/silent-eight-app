d990af9ef00d842d1598551e04f3a28362b4a115
marek sitarski <marek_sitarski@vp.pl>
2021-02-17 16:45:20 +0100

    ref #0006 - Create gender endpoint - getGenderByAlgorithmAndName method

--- src/main/java/com/sitarski/silenteightapp/rest/gender/boundary/GenderRestController.java
+++ src/main/java/com/sitarski/silenteightapp/rest/gender/boundary/GenderRestController.java
@@ -1,4 +1,32 @@
 package com.sitarski.silenteightapp.rest.gender.boundary;
 
+import com.sitarski.silenteightapp.common.entity.AlgorithmType;
+import com.sitarski.silenteightapp.configuration.swagger.entity.SwaggerTags;
+import com.sitarski.silenteightapp.rest.gender.entity.GenderDto;
+import com.sitarski.silenteightapp.service.gender.boundary.GenderService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/api/genders")
+@Api(tags = SwaggerTags.GENDER)
 public class GenderRestController {
+
+    private final GenderService genderService;
+
+    @Autowired
+    public GenderRestController(GenderService genderService) {
+        this.genderService = genderService;
+    }
+
+    @GetMapping(value = "/", produces = "application/json")
+    public GenderDto getGenderByAlgorithmAndName(@RequestParam("algorithmType") AlgorithmType algorithmType, @RequestParam("name") String name) {
+        GenderDto genderDto = genderService.getGenderByAlgorithmAndName(algorithmType, name);
+        return genderDto;
+    }
+
 }
