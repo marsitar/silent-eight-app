27e50a60e816e4669f08885a203b2b273de0626b
marek sitarski <marek_sitarski@vp.pl>
2021-02-17 18:59:37 +0100

    ref #0007 - Gender service refactor - add Algorithm Helper class to provide solution more transparency, add some loggers additionally

--- src/main/java/com/sitarski/silenteightapp/rest/gender/entity/GenderDto.java
+++ src/main/java/com/sitarski/silenteightapp/rest/gender/entity/GenderDto.java
@@ -35,4 +35,11 @@ public class GenderDto {
     public int hashCode() {
         return Objects.hash(genderType);
     }
+
+    @Override
+    public String toString() {
+        return "GenderDto{" +
+                "genderType=" + genderType +
+                '}';
+    }
 }
